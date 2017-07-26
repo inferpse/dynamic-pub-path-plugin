e4fda33e1f4ebfcce34c993972dfebced98b5499
Sergey <inferpse@users.noreply.github.com>
2017-07-26 12:47:33 +0300

    added source-map support

--- index.js
+++ index.js
@@ -1,63 +1,133 @@
-var RawSource = require('webpack-sources').RawSource;
+const { SourceMapSource, RawSource } = require('webpack-sources'),
+      babel = require('babel-core');
 
-function DynamicPubPathPlugin(options) {
-  this.options = options || {};
+class DynamicPubPathPlugin {
+  constructor(options) {
+    this.options = Object.assign({
+      jsregex: /\.js($|\?)/i,
+      expression: null
+    }, options);
+  }
+  apply(compiler) {
+    const { options } = this,
+          { jsregex } = options,
+          useSourceMap = typeof options.sourceMap === 'undefined' ? !!compiler.options.devtool : options.sourceMap;
+
+    compiler.plugin('compilation', function (compilation) {
+
+      if (!options.expression) {
+        compilation.errors.push(new Error('DynamicPubPathPlugin: public path expression is not specified in plugin options!'));
+        return;
+      }
+
+      if (useSourceMap) {
+        compilation.plugin('build-module', function (module) {
+          module.useSourceMap = true;
+        });
+      }
+
+      compilation.plugin('optimize-chunk-assets', function (chunks, callback) {
+        const files = [];
+
+        chunks.forEach(chunk => {
+          chunk.files.forEach(file => files.push(file));
+        });
+
+        compilation.additionalChunkAssets.forEach(file => files.push(file));
+
+        files.filter(file => jsregex.test(file)).forEach(file => {
+          try {
+            let asset = compilation.assets[file];
+
+            // use cached asset
+            if (asset.__dynPubPathApplied) {
+              compilation.assets[file] = asset.__dynPubPathApplied;
+              return;
+            }
+
+            // read options
+            let input, inputSourceMap;
+            if (useSourceMap) {
+              if (asset.sourceAndMap) {
+                let sourceAndMap = asset.sourceAndMap();
+                inputSourceMap = sourceAndMap.map;
+                input = sourceAndMap.source;
+              } else {
+                inputSourceMap = asset.map();
+                input = asset.source();
+              }
+            } else {
+              input = asset.source();
+            }
+
+            // apply transformation
+            const result = babel.transform(input, {
+              plugins: [
+                [TransformWebpackPublicPath, options]
+              ],
+              sourceMaps: useSourceMap,
+              compact: false,
+              babelrc: false,
+              inputSourceMap
+            });
+
+            // save result
+            asset.__dynPubPathApplied = compilation.assets[file] = (
+              result.map
+              ? new SourceMapSource(result.code, file, result.map, input, inputSourceMap)
+              : new RawSource(result.code)
+            );
+          } catch (e) {
+            compilation.errors.push(e);
+          }
+        });
+
+        callback();
+      })
+    });
+  }
 }
 
-DynamicPubPathPlugin.prototype.apply = function(compiler) {
-  var options = this.options;
-  var jsregex = options.test || /\.js($|\?)/i;
-
-  compiler.plugin('compilation', function (compilation) {
-    compilation.plugin('optimize-chunk-assets', function (chunks, callback) {
-      const files = [];
-
-      chunks.forEach(function(chunk) {
-        chunk.files.forEach(function(file) {
-          files.push(file);
-        });
-      });
-
-      compilation.additionalChunkAssets.forEach(function(file) {
-        files.push(file);
-      });
-
-      files.filter(function(file) {
-        return jsregex.test(file);
-      }).forEach(function(file) {
-        try {
-          var asset = compilation.assets[file];
-
-          // return cached version
-          if (asset.__pubPathApplied) {
-            compilation.assets[file] = asset.__pubPathApplied;
-            return;
-          }
-
-          // grab source input
-          var input = asset.source(),
-              result;
-
-          // replace define and requires
-          if (options.expression) {
-            result = input
-              .replace(/__webpack_require__.p = (.*?)\;/, '__webpack_require__.p = function() { return ' + options.expression + ' };')
-              .replace(/__webpack_require__.p \+/g, '__webpack_require__.p() +');
+const TransformWebpackPublicPath = ({types: t}) => {
+  return {
+    visitor: {
+      Identifier: (path, {opts: options}) => {
+        if (isWebpackPathIdentifier(path)) {
+          if (isPathInitExpression(path)) {
+            // replace "__webpack_require__.p = ..." with "__webpack_require__.p = function() { return ... }"
+            if (!isPathInitReplaced(path)) {
+              const assignmentPath = path.parentPath.parentPath;
+              assignmentPath.replaceWithSourceString(`__webpack_require__.p = function() { return ${options.expression} }`);
+            }
           } else {
-            console.log('WARNING dynamic-pub-path-plugin: expression not specified!');
-            result = input;
+            // replace "__webpack_require__.p" with "__webpack_require__.p()"
+            if (!isPathUseReplaced(path)) {
+              path.parentPath.replaceWith(t.callExpression(path.parentPath.node, []));
+            }
           }
-
-          // save result
-          asset.__pubPathApplied = compilation.assets[file] = new RawSource(result);
-        } catch(e) {
-          compilation.errors.push(e);
         }
-      });
+      }
+    }
+  };
 
-      callback();
-    });
-  });
-};
+  function isWebpackPathIdentifier(path) {
+    const { node, parentPath } = path;
+    return node.name === 'p'
+            && parentPath.isMemberExpression()
+            && parentPath.get('object').node.name == '__webpack_require__';
+  }
+
+  function isPathInitExpression(path) {
+    return path.parentPath.parentPath.isAssignmentExpression();
+  }
+
+  function isPathInitReplaced(path) {
+    return path.parentPath.parentPath.node.right.type === 'FunctionExpression';
+  }
+
+  function isPathUseReplaced(path) {
+    return path.parentPath.parentPath.node.type === 'CallExpression';
+  }
+}
 
 module.exports = DynamicPubPathPlugin;
