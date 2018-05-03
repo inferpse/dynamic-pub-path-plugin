3d2a11abe1fd9d1bab857012c74af134429d70b8
Sergey <inferpse@users.noreply.github.com>
2018-05-03 10:29:40 +0300

    updated for webpack 4

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ class DynamicPubPathPlugin {
   }
   apply(compiler) {
     const { expression } = this.options;
-    compiler.plugin('compilation', compilation => {
-      compilation.mainTemplate.plugin('require-extensions', source => {
+    compiler.hooks.compilation.tap('DynamicPubPathPlugin', compilation => {
+      compilation.mainTemplate.hooks.requireExtensions.tap('DynamicPubPathPlugin', source => {
         return source.replace(/(__webpack_require__\.p) = (.*);/, `$1 = (function(o){ o.toString = function() { return ${expression} }; return o; }({}))`)
       });
     });
